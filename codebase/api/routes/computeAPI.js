72dfd5eb5654be1eeef8ea03e9f0c4c68115ad4d
v1enna <19vienna20@gmail.com>
2022-04-12 15:54:25 +0200

    Redirect correct data to the frontend

--- codebase/api/routes/computeAPI.js
+++ codebase/api/routes/computeAPI.js
@@ -17,6 +17,8 @@ router.post('/', function (req, res, next) {
 	const strings = req.body.strings;
 	const algo = req.body.algo;
 
+	const ENCODING = 'utf8';
+
 	console.log(
 		'LW-Index > Stringhe della richiesta [' + algo + ']: ' + strings
 	);
@@ -34,11 +36,16 @@ router.post('/', function (req, res, next) {
 		++index;
 	});
 
-	const __IN_FILE_NAME = algo + '__Request';
+	const __IN_FILE_NAME = algo + '-request-' + Date.now();
 	const __IN_EXTENSION = '.fasta';
-	const __IN_DIRECTORY = './algo/' + algo + '/';
+	const __IN_DIRECTORY = './algo/' + algo + '/lw-index/';
+
+	const __OUT_FILE_NAME = Date.now();
+	const __OUT_EXTENSION = '.fasta.out';
+	const __OUT_DIRECTORY = './algo/' + algo + '/lw-index/result/';
 
 	const __IN_FILE = __IN_DIRECTORY + __IN_FILE_NAME + __IN_EXTENSION;
+	const __OUT_FILE = __OUT_DIRECTORY + __OUT_FILE_NAME + __OUT_EXTENSION;
 
 	fs.writeFile(__IN_FILE, FASTA, function (err) {
 		if (err) throw err;
@@ -49,12 +56,6 @@ router.post('/', function (req, res, next) {
 		es. ./algo/scMAW/sc-maw -a DNA -i ./data/3.fasta -o 2.fasta.out -k 2 -K 10
 	*/
 
-	const __OUT_FILE_NAME = Date.now();
-	const __OUT_EXTENSION = '.fasta.out';
-	const __OUT_DIRECTORY = './algo/' + algo + '/result-lw-index/';
-
-	const __OUT_FILE = __OUT_DIRECTORY + __OUT_FILE_NAME + __OUT_EXTENSION;
-
 	fs.writeFile(__OUT_FILE, FASTA, function (err) {
 		if (err) throw err;
 	});
@@ -64,9 +65,9 @@ router.post('/', function (req, res, next) {
 
 	var wsl = spawn('wsl');
 
-	wsl.stdin.setEncoding('utf8');
-	wsl.stdout.setEncoding('utf8');
-	wsl.stderr.setEncoding('utf8');
+	wsl.stdin.setEncoding(ENCODING);
+	wsl.stdout.setEncoding(ENCODING);
+	wsl.stderr.setEncoding(ENCODING);
 
 	console.log('LW-Index > Esecuzione di ' + algo + '\n');
 
@@ -95,14 +96,62 @@ router.post('/', function (req, res, next) {
 
 		console.log('LW-Index > Conversione OUT-JSON');
 
-		const resBody = {
-			strings: ['a', 'b', 'c'],
-			algo: req.body.algo,
-		};
+		/* JSON Format
+		
+		{
+			data: [
+				{
+					string: 'CCG'
+					lw-1: 0.26
+					lw-2: 0.9
+					...
+					lw-n: 1.6
+				},
 
-		/* Spedisco la risposta al client */
+				{
+					string: 'AAGGT'
+					lw[n]: 2.1
+				},
+			]
+		} */
 
-		res.json(resBody);
+		fs.readFile(__OUT_FILE, ENCODING, (err, data) => {
+			if (err) throw err;
+
+			var result = [];
+
+			// Trasforma il file in righe in modo da poterle leggere comodamente
+
+			const rows = data.split(/\r?\n/);
+
+			// La prima e l'ultima riga sono inutili
+
+			rows.shift();
+			rows.pop();
+
+			rows.forEach((row) => {
+				console.log('LW-Index > Conversione riga ' + row);
+
+				const splitData = row.split('\t');
+
+				let string = splitData[0];
+				let lw = [];
+
+				splitData.shift();
+				splitData.pop();
+				splitData.forEach((data) => lw.push(data));
+
+				result.push({
+					string: string,
+					lw: lw,
+				});
+			});
+
+			/* Spedisco la risposta al client */
+
+			console.log(result);
+			res.json(result);
+		});
 	});
 });
 
